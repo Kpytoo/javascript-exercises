55cc1fef0c84b574f2773418393b215712bb269e
Kpytoo <88066233+Kpytoo@users.noreply.github.com>
2025-05-04 14:50:26 -0400

    Completed 02_repeatString!

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,18 @@
-const repeatString = function() {
+const repeatString = function(stringInput, numberInput) { 
+    let string = "";
+    for (let i = 0; i < numberInput; i++)
+    {
+        string = string + stringInput;
+    }
+    if (numberInput < 0)
+    {
+        return "ERROR";
+    }
+    else
+    {
+        return string;  
+    }
+    
 
 };
 
