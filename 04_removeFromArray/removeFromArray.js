5fe378e0307b940125a3888a9bc119d9941d000f
Kpytoo <88066233+Kpytoo@users.noreply.github.com>
2025-05-11 20:14:09 -0400

    Completed 04_removeFromArray!

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,16 @@
-const removeFromArray = function() {
+
+
+
+const removeFromArray = function(array, ...otherArgs) {
+    
+    for(let i = 0; i < otherArgs.length; i++)
+    {
+        array = array.filter(item => item !== otherArgs[i]);
+    }
+
+    console.log(array)
+    return array;
+
 };
 
 // Do not edit below this line
