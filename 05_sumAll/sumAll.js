052ceeda28fda85ca002cad14865d4b36f4770cc
Kpytoo <88066233+Kpytoo@users.noreply.github.com>
2025-05-11 20:36:01 -0400

    Completed 05_sumAll!

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,28 @@
-const sumAll = function() {
+const sumAll = function(intStart, intEnd) {
+
+    if(intStart < 0 || intEnd < 0 || !(Number.isInteger(intStart)) || !(Number.isInteger(intEnd)))
+    {
+        return "ERROR";
+    }
+
+    let sum = 0;
+    if(intStart < intEnd)
+    {
+        for (let i = intStart; i <= intEnd; i++)
+        {
+            sum += i;
+        }
+    }
+    else
+    {
+        for (let i = intEnd; i <= intStart; i++)
+        {
+            sum += i;
+        }
+    }
+
+    return sum;
+    
 
 };
 
