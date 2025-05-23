d9b36a5393cecd20de886a837ff5872daa3df51a
Kpytoo <88066233+Kpytoo@users.noreply.github.com>
2025-05-23 11:01:32 -0400

    Completed 08_calculator!

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(a,b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((acc,val) => acc + val, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((acc,val) => acc * val, 1);
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let total = 1;
+  for(a; a > 1; a--){
+    total = total * a * (--a);
+  }
+  return total;
 };
 
 // Do not edit below this line
