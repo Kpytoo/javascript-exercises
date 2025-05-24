aeab26a93db989d715eceb7f0011a4b020194d61
Kpytoo <88066233+Kpytoo@users.noreply.github.com>
2025-05-24 15:33:55 -0400

    Completed 12_findTheOldest!

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+let getAge = (person) => {
+    let birth = person.yearOfBirth;
+    let death = person.yearOfDeath;
 
+    if(!death){ // If death is "undefined", then "!undefined" results in "true"
+        death = new Date().getFullYear(); // Assign the current year to "death"
+    } 
+
+    return death - birth; 
+};
+
+
+const findTheOldest = function(array) {
+    return array.reduce((oldest, current) => {
+        let oldestAge = getAge(oldest);
+        let currentAge = getAge(current);
+        return currentAge > oldestAge ? current : oldest;
+    });
 };
 
 // Do not edit below this line
