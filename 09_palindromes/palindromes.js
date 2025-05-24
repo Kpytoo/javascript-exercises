b4c5f9348e28cf0584fa3b83a1a08cfb3ac11176
Kpytoo <88066233+Kpytoo@users.noreply.github.com>
2025-05-24 10:39:40 -0400

    Completed 09_palindromes!

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
+const validCharacters = "abcdefghijklmnopqrstuvwxyz0123456789";
 
+const palindromes = function (str) {
+    let filteredString = str.toLowerCase().split("").filter(char => validCharacters.includes(char)).join("");
+    let reversedString = filteredString.split("").reverse().join("");
+    return (filteredString === reversedString);
 };
 
 // Do not edit below this line
